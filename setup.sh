aae1a4794032780dcdc663933da0f786ffe6ec66
Voinea Radu <contact@voinearadu.com>
2024-09-15 13:39:01 +0300

    Implemented the build process for a custom os (rust_os). Implemented a very basic VGA handler to display some custom ascii art

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-rustup target add thumbv7em-none-eabihf # Telling rust to not link for any specific OS
+rustup target add thumbv7em-none-eabihf
+
+rustup component add rust-src
+rustup target add x86_64-unknown-linux-gnu
+rustup target add nightly-x86_64-unknown-linux-gnu
+
+cargo install bootimage
+rustup component add llvm-tools-preview
+
+apt install qemu-system-x86

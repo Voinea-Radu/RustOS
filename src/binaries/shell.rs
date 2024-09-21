5e42a799e29c7458eb320db4484bcc37fd0fc2b0
Voinea Radu <contact@voinearadu.com>
2024-09-21 17:53:54 +0300

    Implemented a better shell

--- src/binaries/shell.rs
+++ src/binaries/shell.rs
@@ -1,20 +1,60 @@
 use crate::driver::keyboard::KeyboardListener;
-use crate::print;
+use crate::{print, println};
 use pc_keyboard::KeyCode;
 
-pub struct Shell {}
+pub struct Shell {
+    internal_buffer: [char; 256],
+    internal_buffer_index: usize,
+}
 
 impl KeyboardListener for Shell {
-    fn on_key(&self, _key: KeyCode) {}
+    fn handle_key(&mut self, key: KeyCode) {
+        match key {
+            KeyCode::Return => {
+                self.handle_return();
+                return;
+            }
+            _ => {}
+        }
+    }
 
-    fn on_char(&self, char: char) {
+    fn handle_char(&mut self, char: char) {
+        if char == '\n' {
+            self.handle_return();
+            return;
+        }
+
+        if char as u8 == 0x8{
+            self.handle_backspace();
+            return;
+        }
+
+        self.internal_buffer[self.internal_buffer_index] = char;
+        self.internal_buffer_index += 1;
         print!("{}", char)
     }
 }
 
 impl Shell {
     pub fn new() -> Shell {
-        Shell {}
+        Shell {
+            internal_buffer: [0 as char; 256],
+            internal_buffer_index: 0,
+        }
+    }
+
+    pub fn handle_backspace(&mut self) {
+        self.internal_buffer_index -= 1;
+    }
+
+    pub fn handle_return(&mut self) {
+        print!("\nExecuting: ");
+        for index in 0..self.internal_buffer_index {
+            let char = self.internal_buffer[index];
+            print!("{}", char)
+        }
+        self.internal_buffer_index = 0;
+        println!()
     }
 }
 

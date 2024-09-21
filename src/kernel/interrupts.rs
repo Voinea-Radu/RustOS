7e0b8ab654870a6a9b59c8ec7e55d266aa98c3f8
Voinea Radu <contact@voinearadu.com>
2024-09-21 19:00:44 +0300

    Implemented the page fault exception handler

--- src/kernel/interrupts.rs
+++ src/kernel/interrupts.rs
@@ -1,10 +1,11 @@
 use crate::driver::keyboard::keyboard_interrupt_handler;
 use crate::kernel::global_descriptor_table::DOUBLE_FAULT_IST_INDEX;
-use crate::println;
+use crate::{hlt_loop, println};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use spin::Mutex;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::registers::control::Cr2;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
@@ -14,6 +15,8 @@ lazy_static! {
         let mut idt = InterruptDescriptorTable::new();
 
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
         idt[InterruptIndex::Timer as u8].set_handler_fn(timer_handler);
         idt[InterruptIndex::Keyboard as u8].set_handler_fn(keyboard_interrupt_handler);
 
@@ -41,10 +44,7 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("Breakpoint hit:\n{:#?}", stack_frame);
 }
 
-extern "x86-interrupt" fn double_fault_handler(
-    stack_frame: InterruptStackFrame,
-    _error_code: u64,
-) -> ! {
+extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
     panic!("\nDouble fault:\n{:#?}", stack_frame);
 }
 
@@ -54,4 +54,15 @@ extern "x86-interrupt" fn timer_handler(_stack_frame: InterruptStackFrame) {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer as u8)
     }
+}
+
+extern "x86-interrupt" fn page_fault_handler(stack_frame: InterruptStackFrame, error_code: PageFaultErrorCode) {
+    // print!(".");
+
+    println!("Exception        : Page Fault");
+    println!("Accessed address : {:?}", Cr2::read());
+    println!("ErrorCode        : {:?}", error_code);
+    println!("{:#?}", stack_frame);
+
+    hlt_loop();
 }
